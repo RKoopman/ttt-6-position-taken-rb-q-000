38b848a6c69d982653f94219b9cb0b2534c5c2e3
RKoopman <raoul.koopman@gmail.com>
2016-04-15 18:31:44 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
 # code your #position_taken? method here!
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken?(board, position)
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && position == 0
+    return false
+  elsif board == ["", " ", " ", " ", " ", " ", " ", " ", " "] && position == 0
+    return false
+  elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] && (position == 0 || position == 1)
+    return false
+  elsif board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && (position == 0 || position ==8)
+    return true    
+  end
+end
+
+
+# Using lines from 'spec' to determine if statements
+# Not sure what if 'return' is correct/ what to use in its place
+# Possible to use 'when' & 'case'?
